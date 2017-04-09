cdbed92ac4765aa79dc1d308d54be256a769fc96
JS Morisset <jsm@surniaulula.com>
2017-04-09 11:29:06 +0200

    v1.0.4

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
 // extract of translatable text strings from static config array(s)
 _x( 'Rating / Review', 'lib file description', 'wpsso-ratings-and-reviews' );
-_x( 'WPSSO extension to add ratings and reviews for WordPress comments, with aggregate rating meta tags and (optional) Schema Review markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
+_x( 'WPSSO extension to add ratings and reviews for WordPress comments, with Aggregate Rating meta tags and optional Schema Review markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
 ?>
--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -9,7 +9,7 @@
  * Author URI: https://surniaulula.com/
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
- * Description: WPSSO extension to add ratings and reviews for WordPress comments, with aggregate rating meta tags and (optional) Schema Review markup.
+ * Description: WPSSO extension to add ratings and reviews for WordPress comments, with Aggregate Rating meta tags and optional Schema Review markup.
  * Requires At Least: 3.7
  * Tested Up To: 4.7.3
  * Version: 1.0.4
