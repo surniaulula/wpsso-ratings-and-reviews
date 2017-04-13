29ea98e70a9bd39db20b974dbe66e554e048e230
JS Morisset <jsm@surniaulula.com>
2017-04-13 09:11:50 +0200

    v1.0.5-b.1

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -12,7 +12,7 @@
  * Description: WPSSO extension to add ratings and reviews for WordPress comments, with Aggregate Rating meta tags and optional Schema Review markup.
  * Requires At Least: 3.7
  * Tested Up To: 4.7.3
- * Version: 1.0.5-a.2
+ * Version: 1.0.5-b.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
