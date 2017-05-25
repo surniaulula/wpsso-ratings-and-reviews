486e55fc52fa1206c410a6ad44dd91b6393e2eb7
JS Morisset <jsm@surniaulula.com>
2017-05-25 04:53:53 +0200

    v1.0.6

---
+++ uninstall.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl.txt
+ * Copyright 2012-2017 Jean-Sebastien Morisset (https://surniaulula.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) || 
+	! defined( 'WP_UNINSTALL_PLUGIN' ) )
+		die( 'These aren\'t the droids you\'re looking for...' );
+
+$plugin_filepath = dirname( __FILE__ ).'/wpsso-schema-json-ld.php';
+
+require_once dirname( __FILE__ ).'/lib/config.php';
+
+WpssoRarConfig::set_constants( $plugin_filepath );
+WpssoRarConfig::require_libs( $plugin_filepath );	// includes the register.php class library
+WpssoRarRegister::network_uninstall();
+
+?>
