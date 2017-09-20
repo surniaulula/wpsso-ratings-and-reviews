05ee54f8310f296e814d000708b78c5fd74da260
JS Morisset <jsm@surniaulula.com>
2017-09-20 09:16:42 -0400

    v1.0.7

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -12,7 +12,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to add ratings and reviews for WordPress comments, with Aggregate Rating meta tags and optional Schema Review markup.
  * Requires At Least: 3.7
- * Tested Up To: 4.8.1
+ * Tested Up To: 4.8.2
  * Requires PHP: 5.3
  * Version: 1.0.7
  *
