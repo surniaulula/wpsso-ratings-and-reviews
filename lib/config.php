e95c2135641f302b34c5049241ee83f9244d7726
JS Morisset <jsm@surniaulula.com>
2019-03-31 16:58:33 -0600

    v1.5.2-b.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoRarConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssorar' => array(			// Plugin acronym.
-					'version'     => '1.5.2-dev.6',	// Plugin version.
+					'version'     => '1.5.2-b.1',	// Plugin version.
 					'opt_version' => '5',		// Increment when changing default option values.
 					'short'       => 'WPSSO RAR',	// Short plugin name.
 					'name'        => 'WPSSO Ratings and Reviews',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoRarConfig' ) ) {
 					'req' => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '4.26.0-dev.6',
+						'min_version' => '4.26.0-b.1',
 					),
 					'img' => array(
 						'icons' => array(
