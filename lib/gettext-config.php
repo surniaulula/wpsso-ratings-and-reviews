7d83a1d4fd5adec226259baaeedfa9b87e03542a
JS Morisset <jsm@surniaulula.com>
2019-08-02 14:28:24 -0700

    v2.2.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable text strings from the static config array.
  */
 _x( 'Ratings / Reviews', 'lib file description', 'wpsso-ratings-and-reviews' );
-_x( 'WPSSO Core add-on provides ratings and reviews for WordPress comments with Aggregate Rating meta tags and Schema Review markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
+_x( 'Ratings and reviews for WordPress comments with Aggregate Rating meta tags and Schema Review markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
