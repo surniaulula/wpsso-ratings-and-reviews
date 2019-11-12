e79c8104691dfe65ee56391be56430081596a730
JS Morisset <jsm@surniaulula.com>
2019-11-12 06:23:26 -0800

    v2.3.1

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -13,7 +13,7 @@
  * Description: Ratings and reviews for WordPress comments with Aggregate Rating meta tags and Schema Review markup.
  * Requires At Least: 3.9
  * Tested Up To: 5.3
- * WC Tested Up To: 3.7.1
+ * WC Tested Up To: 3.8.0
  * Version: 2.3.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
