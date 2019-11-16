0440ece9956c7165aca5081c872d7d36c6722be6
JS Morisset <jsm@surniaulula.com>
2019-11-16 14:28:59 -0800

    v2.3.1

--- uninstall.php
+++ uninstall.php
@@ -6,14 +6,17 @@
  */
 
 if ( ! defined( 'ABSPATH' ) || ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	die( 'These aren\'t the droids you\'re looking for...' );
+	die( 'These aren\'t the droids you\'re looking for.' );
 }
 
 $plugin_dir = trailingslashit( dirname( __FILE__ ) );
+
 $plugin_filepath = $plugin_dir . 'wpsso-ratings-and-reviews.php';
 
 require_once $plugin_dir . 'lib/config.php';
 
 WpssoRarConfig::set_constants( $plugin_filepath );
+
 WpssoRarConfig::require_libs( $plugin_filepath );	// Includes the register.php class library.
+
 WpssoRarRegister::network_uninstall();
