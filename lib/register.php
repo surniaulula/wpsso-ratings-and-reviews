5b4fab3dc5048f96f20386ad4ef7a953e786ee7f
JS Morisset <jsm@surniaulula.com>
2019-11-21 14:13:23 -0800

    v2.3.2-dev.2

--- lib/register.php
+++ lib/register.php
@@ -99,11 +99,14 @@ if ( ! class_exists( 'WpssoRarRegister' ) ) {
 
 			if ( class_exists( 'Wpsso' ) ) {
 
-				if ( class_exists( 'WpssoUtil' ) ) {	// Just in case.
+				/**
+				 * Register plugin install, activation, update times.
+				 */
+				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.0.
 
 					$version = WpssoRarConfig::$cf[ 'plugin' ][ 'wpssorar' ][ 'version' ];
 
-					WpssoUtil::register_ext_version( 'wpssorar', $version );
+					WpssoUtilReg::update_ext_version( 'wpssorar', $version );
 				}
 			}
 		}
