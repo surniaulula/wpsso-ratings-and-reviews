7ee1e0b571ea9e129ec592edfbd45b5541a3318e
JS Morisset <jsm@surniaulula.com>
2019-12-07 07:28:06 -0800

    v2.4.0

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -11,6 +11,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Ratings and reviews for WordPress comments with Aggregate Rating meta tags and Schema Review markup.
+ * Requires PHP: 5.5
  * Requires At Least: 3.9
  * Tested Up To: 5.3
  * WC Tested Up To: 3.8.1
