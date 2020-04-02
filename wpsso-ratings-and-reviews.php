0e10d4de1151fc945d46976653474ac4f67597d8
JS Morisset <jsm@surniaulula.com>
2020-04-02 14:54:49 -0700

    v2.6.0-dev.1

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -67,6 +67,9 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 			$this->reg = new WpssoRarRegister();		// Activate, deactivate, uninstall hooks.
 
+			/**
+			 * Check for required plugins and show notices.
+			 */
 			add_action( 'all_admin_notices', array( __CLASS__, 'show_required_notices' ) );
 
 			/**
@@ -92,6 +95,9 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 			return self::$instance;
 		}
 
+		/**
+		 * Check for required plugins and show notices.
+		 */
 		public static function show_required_notices() {
 
 			$info = WpssoRarConfig::$cf[ 'plugin' ][ 'wpssorar' ];
@@ -114,10 +120,10 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 				self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
 
-				$error_msg = __( 'The %1$s add-on requires the %2$s plugin &mdash; install and activate the plugin or <a href="%3$s">deactivate this add-on</a>.', 'wpsso-am' );
+				$notice_msg = __( 'The %1$s add-on requires the %2$s plugin &mdash; install and activate the plugin or <a href="%3$s">deactivate this add-on</a>.', 'wpsso-ratings-and-reviews' );
 
 				echo '<div class="notice notice-error error"><p>';
-				echo sprintf( $error_msg, $info[ 'name' ], $req_info[ 'name' ], $deactivate_url );
+				echo sprintf( $notice_msg, $info[ 'name' ], $req_info[ 'name' ], $deactivate_url );
 				echo '</p></div>';
 			}
 		}
@@ -248,11 +254,11 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 			$req_info = $info[ 'req' ][ 'wpsso' ];
 
-			$error_msg = sprintf( __( 'The %1$s version %2$s add-on requires %3$s version %4$s or newer (version %5$s is currently installed).',
+			$notice_msg = sprintf( __( 'The %1$s version %2$s add-on requires %3$s version %4$s or newer (version %5$s is currently installed).',
 				'wpsso-ratings-and-reviews' ), $info[ 'name' ], $info[ 'version' ], $req_info[ 'name' ], $req_info[ 'min_version' ],
 					$this->p->cf[ 'plugin' ][ 'wpsso' ][ 'version' ] );
 
-			$this->p->notice->err( $error_msg );
+			$this->p->notice->err( $notice_msg );
 
 			if ( method_exists( $this->p->admin, 'get_check_for_updates_link' ) ) {
 
