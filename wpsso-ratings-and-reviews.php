cbd2a8d6930e437ee74354f5b0c83741b0b0e92b
JS Morisset <jsm@surniaulula.com>
2020-04-06 09:20:05 -0700

    v2.6.0-rc.3

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -164,6 +164,9 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 		}
 
 		/**
+		 * The 'wpsso_init_options' action is run after the $check, $avail, $debug, $notice, $cache, $util, and $opt
+		 * properties are defined.
+		 *
 		 * Sets the $this->p reference variable for the core plugin instance.
 		 */
 		public function wpsso_init_options() {
