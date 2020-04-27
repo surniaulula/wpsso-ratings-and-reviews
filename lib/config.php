6681429ae6ebf077ae12299c0dde4aa374bf9598
JS Morisset <jsm@surniaulula.com>
2020-04-27 15:15:34 -0700

    v2.7.0

--- lib/config.php
+++ lib/config.php
@@ -52,10 +52,6 @@ if ( ! class_exists( 'WpssoRarConfig' ) ) {
 					 * Library files loaded and instantiated by WPSSO.
 					 */
 					'lib' => array(
-						'pro' => array(
-						),
-						'std' => array(
-						),
 						'submenu' => array(
 							'rar-general' => 'Ratings / Reviews',
 						),
