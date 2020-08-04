09ab6ccc86edba15b9cba669665d1c3c537940de
JS Morisset <jsm@surniaulula.com>
2020-08-04 12:48:02 -0700

    v2.9.0

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -135,7 +135,7 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 			static $loaded = null;
 
-			if ( null !== $loaded ) {
+			if ( null !== $loaded && ! $debug_enabled ) {
 
 				return;
 			}
@@ -273,15 +273,7 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 			$local_cache = array();
 
-			self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
-
 			$info = WpssoRarConfig::$cf[ 'plugin' ][ self::$ext ];
-
-			$plugin_missing_transl = __( 'The %1$s version %2$s add-on requires the %3$s plugin &mdash; please activate the missing plugin.',
-				'wpsso-ratings-and-reviews' );
-
-			$old_version_transl = __( 'The %1$s version %2$s add-on requires %3$s version %4$s or newer (version %5$s is currently installed).',
-				'wpsso-ratings-and-reviews' );
 
 			foreach ( $info[ 'req' ] as $key => $req_info ) {
 
@@ -304,7 +296,11 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 				} elseif ( ! empty( $req_info[ 'plugin_class' ] ) && ! class_exists( $req_info[ 'plugin_class' ] ) ) {
 
-					$req_info[ 'notice' ] = sprintf( $plugin_missing_transl, $info[ 'name' ], $info[ 'version' ], $req_name );
+					self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
+
+					$notice_msg = __( 'The %1$s version %2$s add-on requires the %3$s plugin &mdash; please activate the missing plugin.', 'wpsso-ratings-and-reviews' );
+
+					$req_info[ 'notice' ] = sprintf( $notice_msg, $info[ 'name' ], $info[ 'version' ], $req_name );
 				}
 
 				if ( ! empty( $req_info[ 'version' ] ) ) {
@@ -313,8 +309,11 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 
 						if ( version_compare( $req_info[ 'version' ], $req_info[ 'min_version' ], '<' ) ) {
 
-							$req_info[ 'notice' ] = sprintf( $old_version_transl, $info[ 'name' ], $info[ 'version' ],
-								$req_name, $req_info[ 'min_version' ], $req_info[ 'version' ] );
+							self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
+
+							$notice_msg = __( 'The %1$s version %2$s add-on requires %3$s version %4$s or newer (version %5$s is currently installed).', 'wpsso-ratings-and-reviews' );
+
+							$req_info[ 'notice' ] = sprintf( $notice_msg, $info[ 'name' ], $info[ 'version' ], $req_name, $req_info[ 'min_version' ], $req_info[ 'version' ] );
 						}
 					}
 				}
