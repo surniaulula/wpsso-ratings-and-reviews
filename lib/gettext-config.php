25e5bf495cbe0dbdd3df9393515a5ddcee524c11
JS Morisset <jsm@surniaulula.com>
2020-09-13 10:53:08 -0700

    v2.10.0

--- lib/gettext-config.php
+++
@@ -1,7 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable text strings from the static config array.
- */
-_x( 'Ratings / Reviews', 'lib file description', 'wpsso-ratings-and-reviews' );
-_x( 'Ratings and Reviews for WordPress Comments with Schema Aggregate Rating and Schema Review Markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
