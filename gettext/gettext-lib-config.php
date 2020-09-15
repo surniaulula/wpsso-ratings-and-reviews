b49347794a019a7b0064c21022e03541e652bd92
JS Morisset <jsm@surniaulula.com>
2020-09-15 09:18:29 -0700

    v2.10.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,4 +4,5 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'Ratings / Reviews', 'lib file description', 'wpsso-ratings-and-reviews' );
+_x( 'WPSSO Ratings and Reviews', 'plugin name', 'wpsso-ratings-and-reviews' );
 _x( 'Ratings and Reviews for WordPress Comments with Schema Aggregate Rating and Schema Review Markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
