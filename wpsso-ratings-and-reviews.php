60e40c57b96736c17e28a91ae7fd3a1275e415db
JS Morisset <jsm@surniaulula.com>
2020-12-08 06:02:16 -0800

    v2.12.0

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -12,7 +12,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Ratings and Reviews for WordPress Comments with Schema Aggregate Rating and Schema Review Markup.
  * Requires PHP: 5.6
- * Requires At Least: 4.4
+ * Requires At Least: 4.5
  * Tested Up To: 5.6
  * WC Tested Up To: 4.7.1
  * Version: 2.12.0
