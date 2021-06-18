a1901c56ba7460ad2309d19804291710df26fa22
JS Morisset <jsm@surniaulula.com>
2021-06-18 07:55:22 -0700

    v2.14.1

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -15,7 +15,7 @@
  * Requires At Least: 4.5
  * Tested Up To: 5.7.2
  * WC Tested Up To: 5.4.1
- * Version: 2.14.1-rc.1
+ * Version: 2.14.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
