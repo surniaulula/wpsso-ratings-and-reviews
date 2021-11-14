595ee07c4e2634d7afb5c4a4bbdaccf03fac6570
JS Morisset <jsm@surniaulula.com>
2021-11-13 16:35:56 -0800

    v2.15.2

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -71,10 +71,7 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 			load_plugin_textdomain( 'wpsso-ratings-and-reviews', false, 'wpsso-ratings-and-reviews/languages/' );
 		}
 
-		/**
-		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
-		 */
-		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
+		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
 
