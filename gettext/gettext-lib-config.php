e90847fdecc1579e647fd0f5ebb05ff8dae474a2
JS Morisset <jsm@surniaulula.com>
2021-11-13 20:31:54 -0800

    v2.15.3-dev.3

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -6,3 +6,4 @@
 _x( 'Ratings / Reviews', 'lib file description', 'wpsso-ratings-and-reviews' );
 _x( 'WPSSO Ratings and Reviews', 'plugin name', 'wpsso-ratings-and-reviews' );
 _x( 'Ratings and Reviews for WordPress Comments with Schema Aggregate Rating and Schema Review Markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
+_x( 'WPSSO Core', 'plugin name', 'wpsso-ratings-and-reviews' );
