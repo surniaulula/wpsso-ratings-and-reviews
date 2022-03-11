621409e64a9aba1a8bef2db73d40a5d4f7c2095e
JS Morisset <jsm@surniaulula.com>
2022-03-11 05:41:57 -0800

    v2.18.1

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -13,8 +13,8 @@
  * Description: Ratings and Reviews for WordPress Comments with Schema Aggregate Rating and Schema Review Markup.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.9.1
- * WC Tested Up To: 6.3.0
+ * Tested Up To: 5.9.2
+ * WC Tested Up To: 6.3.1
  * Version: 2.18.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
