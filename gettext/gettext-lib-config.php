afe2b3feb85b82791748abff9df79e44ce8ade46
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:05:42 -0800

    v2.19.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'Ratings / Reviews', 'lib file description', 'wpsso-ratings-and-reviews' );
 _x( 'WPSSO Ratings and Reviews', 'plugin name', 'wpsso-ratings-and-reviews' );
 _x( 'Ratings and Reviews for WordPress Comments with Schema Aggregate Rating and Schema Review Markup.', 'plugin description', 'wpsso-ratings-and-reviews' );
