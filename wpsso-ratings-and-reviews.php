f825532dec757fc8bf0d2f6f00fa8ced984e0081
JS Morisset <jsm@surniaulula.com>
2023-02-04 08:08:10 -0800

    v2.20.1

--- wpsso-ratings-and-reviews.php
+++ wpsso-ratings-and-reviews.php
@@ -73,6 +73,9 @@ if ( ! class_exists( 'WpssoRar' ) ) {
 			load_plugin_textdomain( 'wpsso-ratings-and-reviews', false, 'wpsso-ratings-and-reviews/languages/' );
 		}
 
+		/*
+		 * Called by Wpsso->set_objects which runs at init priority 10.
+		 */
 		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
