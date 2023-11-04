ce2c3176c4f7acfde8852878cd2eaa1597f65529
JS Morisset <jsm@surniaulula.com>
2023-11-03 18:25:47 -0700

    v2.24.0-dev.9

--- lib/submenu/ratings-reviews.php
+++ lib/submenu/ratings-reviews.php
@@ -33,7 +33,7 @@ if ( ! class_exists( 'WpssoRarSubmenuRatingsReviews' ) && class_exists( 'WpssoAd
 			);
 		}
 
-		protected function get_table_rows( $page_id, $metabox_id, $tab_key = '' ) {
+		protected function get_table_rows( $page_id, $metabox_id, $tab_key = '', $args = array() ) {
 
 			$table_rows = array();
 			$match_rows = trim( $page_id . '-' . $metabox_id . '-' . $tab_key, '-' );
